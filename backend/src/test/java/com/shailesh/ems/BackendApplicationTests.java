c13ed1fa5c85e6efbab9331990a04b2384d64969
Shailesh kumar <skrshailesh1996@gmail.com>
2024-03-20 00:53:24 +0530

    inital commit

---
+++ backend/src/test/java/com/shailesh/ems/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shailesh.ems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ frontend/src/App.js
@@ -0,0 +1,10 @@
+
+import './App.css';
+
+function App() {
+  
+    <div className="App"></div>
+
+}
+
+export default App;
